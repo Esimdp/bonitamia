0b1ff7ccaef6147d5291c6d41f66547e5418dd7e
Esimdp <61088220+Esimdp@users.noreply.github.com>
2020-12-28 19:05:19 -0300

    Add files via upload

---
+++ imemail/imEmailForm.php
@@ -0,0 +1,20 @@
+<?php
+if(substr(basename($_SERVER['PHP_SELF']), 0, 11) == "imEmailForm") {
+	include '../res/x5engine.php';
+	$form = new ImForm();
+	$form->setField('NOMBRE', @$_POST['imObjectForm_19_1'], '', false);
+	$form->setField('E-MAIL', @$_POST['imObjectForm_19_2'], '', false);
+	$form->setField('CONSULTA', @$_POST['imObjectForm_19_3'], '', false);
+
+	if(@$_POST['action'] != 'check_answer') {
+		if(!isset($_POST['imJsCheck']) || $_POST['imJsCheck'] != 'D433170D9874F88DA7637F84C8D20BA8' || (isset($_POST['imSpProt']) && $_POST['imSpProt'] != ""))
+			die(imPrintJsError());
+		$form->mailToOwner('bonitamiamoda@gmail.com', 'bonitamiamoda@gmail.com', 'consulta web', 'Gracias por enviarnos tu consulta, a la brevedad nos estaremos comunicando contigo.', false);
+		@header('Location: ../index.html');
+		exit();
+	} else {
+		echo $form->checkAnswer(@$_POST['id'], @$_POST['answer']) ? 1 : 0;
+	}
+}
+
+// End of file
---
+++ res/access.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'bd9354ro';
+$imSettings['access']['users'] = array(
+	'esimdp@gmail.com' => array(
+		'groups' => array('rdu2sa48'),
+		'id' => 'a7qqb7pd',
+		'firstname' => 'Admin',
+		'lastname' => '',
+		'password' => '1Wc1mc6LWL3e',
+		'email' => 'esimdp@gmail.com',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('a7qqb7pd');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
